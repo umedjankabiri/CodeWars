0b3f29786f5392e99f883b179f29bc9040eeffd9
UMEDJAN KABIRI <122937043+UMEDJANKABIRI@users.noreply.github.com>
2023-08-27 19:55:32 +0300

    Create countSheeps.js

---
+++ countSheeps.js
@@ -0,0 +1,18 @@
+/*Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+For example,
+
+[true,  true,  true,  false,
+  true,  true,  true,  true ,
+  true,  false, true,  false,
+  true,  false, false, true ,
+  true,  true,  true,  true ,
+  false, false, true,  true]*/
+
+function countSheeps(arrayOfSheep) {
+  var count = 0;
+  for(i = 0; i < arrayOfSheep.length; i++)
+    if (arrayOfSheep[i] == true)
+      count++;
+  return count;
+}
